058cad9974df3992894581507854ed4f7a5a51bc
nav0t <31127422+nav0t@users.noreply.github.com>
2018-02-12 14:23:40 +0100

    Added Texture Switching
    Can now switch between all texture in texture folder. Will automatically find all textures in the folder, and use it with [TextureName]+"_Normals" as normal map

--- Sources/DecouplerShroud/ModuleDecouplerShroud.cs
+++ Sources/DecouplerShroud/ModuleDecouplerShroud.cs
@@ -5,66 +5,98 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace DecouplerShroud
-{
+namespace DecouplerShroud {
 	public class ModuleDecouplerShroud : PartModule {
 
 		const int SIDES = 24;
 
-		[KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false), UI_Toggle(invertButton = true)]
+		[KSPField(guiName = "DecouplerShroud", isPersistant = true, guiActiveEditor = true, guiActive = false), UI_Toggle(invertButton = true)]
 		public bool shroudEnabled = false;
 
-		[KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false), UI_Toggle(invertButton = true)]
+		[KSPField(guiName = "Automatic Shroud Size", isPersistant = true, guiActiveEditor = true, guiActive = false), UI_Toggle(invertButton = true)]
 		public bool autoDetectSize = true;
 
-		[KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = .125f, maxValue = 7f, stepIncrement = 0.125f)]
+		[KSPField(guiName = "Top Radius", isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = .125f, maxValue = 7f, stepIncrement = 0.125f)]
 		public float topRad = 1.25f;
-		[KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = .125f, maxValue = 7f, stepIncrement = 0.125f)]
+		[KSPField(guiName = "Bottom Radius", isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = .125f, maxValue = 7f, stepIncrement = 0.125f)]
 		public float botRad = 1.25f;
-		[KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = (1/64f), maxValue = 1f, stepIncrement = (1/64f))]
+		[KSPField(guiName = "Shroud Thickness", isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = (1 / 64f), maxValue = 1f, stepIncrement = (1 / 64f))]
 		public float width = .1f;
-		[KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = .125f, maxValue = 7f, stepIncrement = 0.125f)]
+		[KSPField(guiName = "Shroud Height", isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = .125f, maxValue = 7f, stepIncrement = 0.125f)]
 		public float height = 1.25f;
-		[KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = -1f, maxValue = 1f, stepIncrement = 0.125f)]
+		[KSPField(guiName = "Vertical Offset", isPersistant = true, guiActiveEditor = true, guiActive = false), UI_FloatRange(minValue = -1f, maxValue = 1f, stepIncrement = 0.125f)]
 		public float bottomStart = 0.0f;
+
+		[KSPField(guiName = "Shroud Texture", isPersistant = true, guiActiveEditor = true, guiActive = false)]
+		[UI_ChooseOption(affectSymCounterparts = UI_Scene.Editor, options = new[] { "None" }, scene = UI_Scene.Editor, suppressEditorShipModified = true)]
+		public int textureIndex;
 
 		ModuleJettison engineShroud;
 		GameObject shroudGO;
 		Material shroudMat;
 		ShroudShaper shroudCylinders;
 
+		static List<SurfaceTexture> surfaceTextures;
+
 		public void setup() {
+
+			getTextures();
 
 			//Set up events
 			part.OnEditorAttach += partReattached;
 			part.OnEditorDetach += partDetached;
 
-			Fields["shroudEnabled"].OnValueModified += activeToggled;
-			Fields["autoDetectSize"].OnValueModified += setButtonActive;
-			Fields["autoDetectSize"].OnValueModified += detectSize;
+			Fields[nameof(shroudEnabled)].OnValueModified += activeToggled;
+			Fields[nameof(autoDetectSize)].OnValueModified += setButtonActive;
+			Fields[nameof(autoDetectSize)].OnValueModified += detectSize;
 
-			Fields["topRad"].OnValueModified += updateShroud;
-			Fields["botRad"].OnValueModified += updateShroud;
-			Fields["height"].OnValueModified += updateShroud;
-			Fields["width"].OnValueModified += updateShroud;
-			Fields["bottomStart"].OnValueModified += updateShroud;
+			Fields[nameof(topRad)].OnValueModified += updateShroud;
+			Fields[nameof(botRad)].OnValueModified += updateShroud;
+			Fields[nameof(height)].OnValueModified += updateShroud;
+			Fields[nameof(width)].OnValueModified += updateShroud;
+			Fields[nameof(bottomStart)].OnValueModified += updateShroud;
+			Fields[nameof(textureIndex)].OnValueModified += updateTexture;
 
 			setButtonActive();
 
 			if (HighLogic.LoadedSceneIsFlight) {
 				createShroudGO();
 			} else {
-				//Check if part is loaded in VAB with parent
-				if (part.parent != null) {
-					createShroudGO();
-				}
+				createShroudGO();
 			}
 		}
 
 
 		public void Start() {
 			setup();
+		}
 
+		void getTextures() {
+			if (surfaceTextures == null) {
+				List<GameDatabase.TextureInfo> textures = GameDatabase.Instance.GetAllTexturesInFolder("DecouplerShroud/Textures/");
+				surfaceTextures = new List<SurfaceTexture>();
+				foreach (GameDatabase.TextureInfo inf in textures) {
+					string name = inf.name.Replace("DecouplerShroud/Textures/", "");
+					if (inf.name.EndsWith("_Normals")) {
+						continue;
+					}
+					Texture tex = inf.texture;
+					Texture nor = GameDatabase.Instance.GetTexture(inf.name + "_Normals", false);
+					surfaceTextures.Add(new SurfaceTexture(name, tex, nor));
+					if (nor == null) {
+						Debug.Log("No Normal map found: " + inf.name + "_Normals");
+					}
+				}
+			}
+
+			string[] options = new string[surfaceTextures.Count];
+			for (int i = 0; i < options.Length; i++) {
+				options[i] = surfaceTextures[i].name;
+			}
+
+			BaseField textureField = Fields["textureIndex"];
+			UI_ChooseOption textureOptions = (UI_ChooseOption)textureField.uiControlEditor;
+			textureOptions.options = options;
 		}
 
 		//Executes when shroud is enabled/disabled
@@ -91,24 +123,40 @@ namespace DecouplerShroud
 		void setButtonActive() {
 
 			if (shroudEnabled) {
-				Fields["autoDetectSize"].guiActiveEditor = true;
+				Fields[nameof(autoDetectSize)].guiActiveEditor = true;
+				Fields[nameof(textureIndex)].guiActiveEditor = true;
+
 			} else {
-				Fields["autoDetectSize"].guiActiveEditor = false;
+				Fields[nameof(autoDetectSize)].guiActiveEditor = false;
+				Fields[nameof(textureIndex)].guiActiveEditor = false;
+
 			}
 			if (shroudEnabled && !autoDetectSize) {
-				Fields["topRad"].guiActiveEditor = true;
-				Fields["botRad"].guiActiveEditor = true;
-				Fields["height"].guiActiveEditor = true;
-				Fields["bottomStart"].guiActiveEditor = true;
-				Fields["width"].guiActiveEditor = true;
+				Fields[nameof(topRad)].guiActiveEditor = true;
+				Fields[nameof(botRad)].guiActiveEditor = true;
+				Fields[nameof(height)].guiActiveEditor = true;
+				Fields[nameof(bottomStart)].guiActiveEditor = true;
+				Fields[nameof(width)].guiActiveEditor = true;
 			} else {
-				Fields["topRad"].guiActiveEditor = false;
-				Fields["botRad"].guiActiveEditor = false;
-				Fields["height"].guiActiveEditor = false;
-				Fields["bottomStart"].guiActiveEditor = false;
-				Fields["width"].guiActiveEditor = false;
+				Fields[nameof(topRad)].guiActiveEditor = false;
+				Fields[nameof(botRad)].guiActiveEditor = false;
+				Fields[nameof(height)].guiActiveEditor = false;
+				Fields[nameof(bottomStart)].guiActiveEditor = false;
+				Fields[nameof(width)].guiActiveEditor = false;
 			}
 
+		}
+
+		void updateTexture(object arg) { updateTexture(); }
+		void updateTexture() {
+
+			Debug.Log("Setting Texture to " + textureIndex);
+			Debug.Log("eq: " + (shroudMat == shroudGO.GetComponent<Renderer>().sharedMaterial));
+			if (shroudMat != shroudGO.GetComponent<Renderer>().sharedMaterial) {
+				shroudMat = shroudGO.GetComponent<Renderer>().sharedMaterial;
+			}
+			shroudMat.SetTexture("_MainTex", surfaceTextures[textureIndex].texture);
+			shroudMat.SetTexture("_BumpMap", surfaceTextures[textureIndex].normalMap);
 		}
 
 		void partReattached() {
@@ -168,6 +216,7 @@ namespace DecouplerShroud
 
 		void destroyShroud() {
 			Destroy(shroudGO);
+			Destroy(shroudMat);
 		}
 
 		//updates the shroud mesh when values changed
@@ -194,24 +243,30 @@ namespace DecouplerShroud
 			shroudCylinders = new ShroudShaper(24);
 			shroudCylinders.generate(bottomStart, height, botRad, topRad, width);
 
+			if (shroudGO != null) {
+				Destroy(shroudGO);
+			}
+
 			shroudGO = new GameObject("DecouplerShroud");
 			shroudGO.transform.parent = transform;
 			shroudGO.transform.localPosition = topNode.position;
 			shroudGO.transform.localRotation = Quaternion.identity;
 			shroudGO.AddComponent<MeshFilter>().sharedMesh = shroudCylinders.multiCylinder.mesh;
 
+			if (shroudMat != null) {
+				Destroy(shroudMat);
+			}
 			shroudMat = CreateMat();
-			shroudGO.AddComponent<MeshRenderer>().material = shroudMat;
+			shroudGO.AddComponent<MeshRenderer>();
+			shroudGO.GetComponent<Renderer>().material = shroudMat;
 		}
-		
+
 		//Creates the material for the mesh
 		Material CreateMat() {
 			Material mat = new Material(Shader.Find("KSP/Bumped Specular"));
-			Texture tex = GameDatabase.Instance.GetTexture("DecouplerShroud/Textures/DecouplerShroud", false);
-			Texture nor = GameDatabase.Instance.GetTexture("DecouplerShroud/Textures/DecouplerShroudNormals", false);
-			
-			mat.SetTexture("_MainTex", tex);
-			mat.SetTexture("_BumpMap", nor);
+
+			mat.SetTexture("_MainTex", surfaceTextures[textureIndex].texture);
+			mat.SetTexture("_BumpMap", surfaceTextures[textureIndex].normalMap);
 
 			mat.SetFloat("_Shininess", .07f);
 			mat.SetColor("_SpecColor", Color.white * (95 / 255f));
